31e3d14cb57f77d7ee944ceb017aa65389a2d12f
Ethress <79015360+Ethress@users.noreply.github.com>
2023-12-14 14:52:58 +0100

    Update main.py, cropping part of Discord app
    Overlaps a transparent tkinter window on Discord +
    Removed the right border + exteriors to change Discord window size
    
    /!\ Still cannot click inside the insisible ovelay

--- Python/Discord-translator-RT/main.py
+++ Python/Discord-translator-RT/main.py
@@ -1,6 +1,9 @@
 ##########################################################
-#    Overlaps a transparent tkinter window on Discord
-#
+#   Overlaps a transparent tkinter window on Discord
+#   Remove the right border + exteriors to change Discord
+#       window size
+#   /!\ Still cannot click inside the insisible ovelay
+#   Todo: Add the translation part
 ##########################################################
 import tkinter as tk
 import pygetwindow as gw
@@ -32,6 +35,8 @@ if __name__ == '__main__':
     label = tk.Label(root, text=text_to_display, font=('Arial', 10), fg='black')    # fg='white', bg='black'
     label = label.place(x=0, y=0)
     root.overrideredirect(True)
+    # print(location)
+    # root.geometry(location)
     root.geometry(f"{discord_window.width}x{discord_window.height}+{discord_window.left}+{discord_window.top}")  # real 1920x1080    # 800x500+250+250
     root.lift()
     root.wm_attributes("-topmost", True)
@@ -40,5 +45,13 @@ if __name__ == '__main__':
 
     while True:
         discord_window = gw.getWindowsWithTitle('Discord')[0]
-        root.geometry(f"{discord_window.width}x{discord_window.height}+{discord_window.left}+{discord_window.top}")
+        location = str(discord_window.width - 390 - 28) + "x" + \
+                   str(discord_window.height - 28 - 28*4) + "+" + \
+                   str(discord_window.left + 390) + "+" + \
+                   str(discord_window.top + 28)
+        root.geometry(location)
+        screenshot = pyautogui.screenshot(
+            region=(discord_window.left + 390, discord_window.top + 28, discord_window.width - 390 - 28, discord_window.height - 28 - 28*4))
+        img_np = np.array(screenshot)
+        cv2.imwrite('processed_image_.png', img_np)
         root.update()
