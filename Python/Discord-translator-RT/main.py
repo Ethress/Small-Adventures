77f3c8698cb87274c93a862dec78692f66193950
Ethress <79015360+Ethress@users.noreply.github.com>
2023-12-15 22:54:10 +0100

    Update main.py added pass-through clicking
    Can click inside the invisible ovelay to interact with lower windows

--- Python/Discord-translator-RT/main.py
+++ Python/Discord-translator-RT/main.py
@@ -2,56 +2,45 @@
 #   Overlaps a transparent tkinter window on Discord
 #   Remove the right border + exteriors to change Discord
 #       window size
-#   /!\ Still cannot click inside the insisible ovelay
+#   Can click inside the invisible ovelay to interact
+#       with lower windows
 #   Todo: Add the translation part
 ##########################################################
-import tkinter as tk
+from tkinter import *
 import pygetwindow as gw
-import pyautogui
-import cv2
-import numpy as np
 
-root = tk.Tk()
-text_to_display = "Hello, Transparent Window!"
 
-window_width = 400
-window_height = 100
+def get_Discord_position():
+    window = gw.getWindowsWithTitle('Discord')[0]
+    return window
 
-def on_after():
-    label.configure(text="hello")
+def get_location_Discord(window_settings):
+    size_left_menu = 310
+    size_border = 28
 
-if __name__ == '__main__':
+    location = str(window_settings.width - size_left_menu) + "x" + \
+               str(window_settings.height - size_border) + "+" + \
+               str(window_settings.left + size_left_menu) + "+" + \
+               str(window_settings.top + size_border)
 
-    discord_window = gw.getWindowsWithTitle('Discord')[0]
-    print(discord_window)
-    """
-    screenshot = pyautogui.screenshot(region=(discord_window.left, discord_window.top, discord_window.width, discord_window.height))
-    img_np = np.array(screenshot)
-    cv2.imwrite('processed_image_.png', img_np)
-    """
+    return location
 
-    root.image = tk.PhotoImage(file='butterfly.gif')    # The image must be stored to Tk or it will be garbage collected.
-    root.configure(bg='')
-    label = tk.Label(root, text=text_to_display, font=('Arial', 10), fg='black')    # fg='white', bg='black'
-    label = label.place(x=0, y=0)
-    root.overrideredirect(True)
-    # print(location)
-    # root.geometry(location)
-    root.geometry(f"{discord_window.width}x{discord_window.height}+{discord_window.left}+{discord_window.top}")  # real 1920x1080    # 800x500+250+250
-    root.lift()
-    root.wm_attributes("-topmost", True)
-    root.wm_attributes("-disabled", True)
-    root.wm_attributes("-transparentcolor", "white")
+root = Tk()
 
-    while True:
-        discord_window = gw.getWindowsWithTitle('Discord')[0]
-        location = str(discord_window.width - 390 - 28) + "x" + \
-                   str(discord_window.height - 28 - 28*4) + "+" + \
-                   str(discord_window.left + 390) + "+" + \
-                   str(discord_window.top + 28)
-        root.geometry(location)
-        screenshot = pyautogui.screenshot(
-            region=(discord_window.left + 390, discord_window.top + 28, discord_window.width - 390 - 28, discord_window.height - 28 - 28*4))
-        img_np = np.array(screenshot)
-        cv2.imwrite('processed_image_.png', img_np)
-        root.update()
+label = Label(root, text="This is a New Line Text", font='Helvetica 12 bold', foreground="black")
+label.place(x=0, y=0)
+
+
+root.wm_attributes('-transparentcolor', '#add123')      # Create a transparent window
+root.wm_attributes("-topmost", True)
+root.overrideredirect(True)
+
+root.geometry("10x10")    # Set the geometry of window
+root.config(bg='#add123')   # Add a background color to the Main Window
+
+while True:
+    discord_window = get_Discord_position()
+    overlay_geometry = get_location_Discord(discord_window)
+    root.geometry(overlay_geometry)
+    # label.place(x=150, y=50)
+    root.update()
